0060346aab6328895af327c25a420b954001bf94
ArthurSonzogni <sonzogniarthur@gmail.com>
2020-05-22 18:03:20 +0200

    Improve the starter project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/screen.hpp"
 #include "ftxui/screen/string.hpp"
@@ -6,33 +7,32 @@
 int main(void) {
   using namespace ftxui;
 
-  auto summary = [&]{
-    return
-      window(text(L" Summary "),
-        vbox(
-          hbox(text(L"- done:   "), text(L"3") | bold) | color(Color::Green),
-          hbox(text(L"- active: "), text(L"2") | bold) | color(Color::RedLight),
-          hbox(text(L"- queue:  "), text(L"9") | bold) | color(Color::Red)
-        )
-      );
+  auto summary = [&] {
+    auto content = vbox({
+        hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
+        hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
+        hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
+    });
+    return window(text(L" Summary "), content);
   };
 
-  auto document =
-    vbox(
-      hbox(
-        summary(),
-        summary(),
-        summary() | flex
-      ),
-      summary(),
-      summary()
-    );
-  
+  auto document =  //
+      vbox({
+          hbox({
+              summary(),
+              summary(),
+              summary() | flex,
+          }),
+          summary(),
+          summary(),
+      });
+
   // Limit the size of the document to 80 char.
-  document = std::move(document) | size(WIDTH, LESS_THAN, 80);
+  document = document | size(WIDTH, LESS_THAN, 80);
 
   auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  Render(screen, document.get());
+  Render(screen, document);
+
   std::cout << screen.ToString() << std::endl;
 
   return EXIT_SUCCESS;
